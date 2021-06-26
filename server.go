6cd0e5a67cf94aaa9af40768433aeea47aab993c
Yuki Ito <mrno110y@gmail.com>
2021-06-26 23:23:13 +0900

    add Server interface

---
+++ server.go
@@ -0,0 +1,17 @@
+package servergroup
+
+import (
+	"context"
+)
+
+type Server interface {
+	Start(context.Context) error
+}
+
+var _ Server = (ServerFunc)(nil)
+
+type ServerFunc func(ctx context.Context) error
+
+func (f ServerFunc) Start(ctx context.Context) error {
+	return f(ctx)
+}
